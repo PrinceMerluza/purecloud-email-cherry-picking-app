8daee3e281156dbbe33bdadff6216bd24bf2f0a8
hero2bash <zero.bash@gmail.com>
2019-09-25 14:14:12 +0800

    Late Initial Commit

---
+++ docs/scripts/main.js
@@ -0,0 +1,143 @@
+import view from './view.js';
+
+const platformClient = require('platformClient');
+const client = platformClient.ApiClient.instance;
+
+// OAuth 
+const redirectUri = window.location.href;
+const clientId = "e7de8a75-62bb-43eb-9063-38509f8c21af";
+const queueId = "9f7ae000-70ce-4afd-9bbd-746f5a4d7163";
+
+// API instances
+const analyticsApi = new platformClient.AnalyticsApi();
+const conversationsApi = new platformClient.ConversationsApi();
+
+client.loginImplicitGrant(clientId, redirectUri)
+.then((data) => {
+    console.log(data);
+
+    // Query open email conversations from the queue
+    return getUnansweredEmailsFromQueue(queueId);
+})
+.then((conversations) => {
+    // mutate the information from emails to prepare for viewing
+    return buildEmailInformation(conversations);
+})
+.then((emails) => {
+    // Show the email info on the document
+    emails.forEach((email) => view.addEmailBox(email));
+})
+.catch((err) => {
+    console.log(err);
+});
+
+function getUnansweredEmailsFromQueue(queueId){
+    let intervalTo = moment().utc().add(1, 'h');
+    let intervalFrom = intervalTo.clone().subtract(7, 'days');
+    let intervalString = intervalFrom.format() + '/' + intervalTo.format();
+    
+    let queryBody = {
+        "interval": intervalString,
+        "order": "asc",
+        "orderBy": "conversationStart",
+        "paging": {
+            "pageSize": "100",
+            "pageNumber": 1
+        },
+        "segmentFilters": [
+            {
+            "type": "and",
+            "predicates": [
+            {
+            "type": "dimension",
+            "dimension": "mediaType",
+            "operator": "matches",
+            "value": "email"
+            },
+            {
+            "type": "dimension",
+            "dimension": "queueId",
+            "operator": "matches",
+            "value": queueId
+            }
+            ]
+            }
+        ],
+        "conversationFilters": [
+            {
+            "type": "or",
+            "predicates": [
+            {
+            "type": "dimension",
+            "dimension": "conversationEnd",
+            "operator": "notExists",
+            "value": null
+            }
+            ]
+            }
+        ]
+    }
+
+    return analyticsApi.postAnalyticsConversationsDetailsQuery(queryBody);
+}
+
+function buildEmailInformation(conversationsData){
+    let emails = [];
+
+    for(let conversation of conversationsData.conversations){
+        emails.push(new Promise((resolve, reject) => {
+            // Default Values
+            let senderName = '<No Name>';
+            let senderEmail = '<No Email>';
+            let emailSubject = '<No Subject>';
+            let emailBody = '<No Body>';
+            
+            // Get duration from conversation start
+            let durationMinutes = moment.duration(
+                moment().utc().diff(moment(conversation.conversationStart))).as('minutes');
+            let daysAgo = Math.floor(durationMinutes / (60 * 24));
+            let hoursAgo = Math.floor((durationMinutes / 60) % 24);
+            let minutesAgo = Math.floor(durationMinutes % 60);
+            let emailDuration = '';
+            if(daysAgo >= 1) emailDuration += daysAgo + 'day(s) ';
+            if(hoursAgo >= 1) emailDuration += hoursAgo + 'hour(s) ';
+            emailDuration += minutesAgo + 'minute(s)';
+
+            // Get message information for Email Subject and Body
+            conversationsApi.getConversationsEmailMessages(conversation.conversationId)
+            .then((messages) => {
+                // Get the latest email message
+                let lastEntryIndex = messages.entities.length - 1;
+                let messageId = messages.entities[lastEntryIndex].id;
+
+                // Assign Subject String
+                emailSubject = messages.entities[lastEntryIndex].subject ? 
+                                messages.entities[lastEntryIndex].subject : emailSubject;
+
+                return conversationsApi.getConversationsEmailMessage(conversation.conversationId, messageId)
+            })
+            .then((message) => {
+                // Assigne email values based from latest message
+                senderName = message.from.name ? message.from.name : senderName;
+                senderEmail = message.from.email ? message.from.email : senderEmail;
+                emailBody = message.textBody ? message.textBody : emailBody;
+            })
+            .then((data) => {
+                resolve({
+                    "senderName": senderName,
+                    "senderEmail": senderEmail,
+                    "emailDuration": emailDuration,
+                    "emailSubject": emailSubject,
+                    "emailBody": emailBody
+                })
+            })
+            .catch((err) => {
+                console.log('Oh Shit');
+                console.error(err);
+                reject(err);
+            });
+        }));        
+    }
+
+    return Promise.all(emails);
+}
---
+++ docs/scripts/view.js
@@ -0,0 +1,50 @@
+const emailContainer = document.getElementById('email-container');
+let emailBoxTemplate = document.createElement('template');
+emailBoxTemplate.innerHTML =
+`<div class="box">
+    <article class="media">
+    <div class="media-content">
+        <div class="content">
+        <p>
+            <strong>From: <span class="sender-name">John Smith</span></strong> <small><span class="sender-email">email@email.com</span></small> <small><span class="email-duration">31m</span></small>
+            <br>
+            <strong>Subject: <span class="email-subject">Subject</span></strong>
+            <br>
+            <div class="email-body">
+                Body of email
+            </div>
+        </p>
+        </div>
+        <div>
+            <a class="button is-dark">Assign To Me</a>
+        </div>
+    </div>
+    </article>
+</div>`;
+
+export default {
+    /**
+     * Add an email box to the document
+     * @param {Object} emailData contains the email information
+     */
+    addEmailBox(emailData){
+        // Add the email box to the DOM
+        let emailView = document.importNode(emailBoxTemplate.content, true);
+        let emailViewElement = emailView.firstChild;
+        emailContainer.appendChild(emailView);
+
+        // Get references to dynamic elements
+        let senderName = emailViewElement.getElementsByClassName('sender-name')[0];
+        let senderEmail = emailViewElement.getElementsByClassName('sender-email')[0];
+        let emailDuration = emailViewElement.getElementsByClassName('email-duration')[0];
+        let emailSubject = emailViewElement.getElementsByClassName('email-subject')[0];
+        let emailBody = emailViewElement.getElementsByClassName('email-body')[0];
+        
+        // Assign values
+        senderName.textContent = emailData.senderName ? emailData.senderName : null;
+        senderEmail.textContent = emailData.senderEmail ? emailData.senderEmail : null;
+        emailDuration.textContent = emailData.emailDuration ? emailData.emailDuration : null;
+        emailSubject.textContent = emailData.emailSubject ? emailData.emailSubject : null;
+        emailBody.textContent = emailData.emailBody ? emailData.emailBody : null;
+    }
+}
